de46698e081285cf1cf8c5b83d9541100f621b1e
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:40:13 -0300

    exercicio_8
    Escreva um programa que calcule o fatorial de um número informado pelo usuário. Dica: O fatorial de um número N é dado pela fórmula: N! = 1 * 2 * 3 * 4 * 5 * ... * N

---
+++ exercicio_8/exercicio_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: \n");
+        int A1 = int.Parse(Console.ReadLine());
+
+        long F = 1;
+
+        for (int i = 1; i <= A1; i++)
+        {
+            F *= i;
+        }
+
+        Console.WriteLine($"\nO fatorial de {A1} é: {F}");
+    }
+}
