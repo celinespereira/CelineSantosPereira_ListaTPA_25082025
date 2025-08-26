c473fb002cc1044b64b32a861d2702f5cc594120
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:36:31 -0300

    exercicio_4
    Escreva um programa que exiba na tela a quantidade de números ímpares existentes entre dois números que o usuário digitar (testar inclusive os números digitados).

---
+++ exercicio_4/exercicio_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        int A1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int A2 = int.Parse(Console.ReadLine());
+
+        int C = 0;
+
+        for (int i = A1; i <= A2; i++)
+        {
+            if (i % 2 != 0)
+            {
+                C++;
+            }
+        }
+
+        Console.WriteLine($"\nQuantidade de números ímpares entre {A1} e {A2}: {C}");
+    }
+}
