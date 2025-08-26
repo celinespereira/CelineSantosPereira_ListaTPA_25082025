a65ce915d471be7bfc490800eeeacf2fded185cf
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:45:06 -0300

    exercicio_10
    Faça um programa que leia as variáveis C e N, respectivamente código e número de horas trabalhadas de um operário. Calcule o salário sabendo-se que ele ganha R$ 10,00 por hora. Quando o número de horas exceder a 50, calcule o excesso de pagamento armazenando-o na variável E, caso contrário zerar tal variável. A hora excedente de trabalho vale R$ 20,00. No final do processamento imprimir o salário total e o salário excedente. O programa só deve parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja encerrar o programa?".

---
+++ exercicio_10/exercicio_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string R;
+
+        do
+        {
+            Console.Write("Digite o código: ");
+            string A1 = Console.ReadLine();
+
+            Console.Write("Digite o número de horas: ");
+            int A2 = int.Parse(Console.ReadLine());
+
+            int E = 0;
+            int B = 0;
+
+            if (A2 > 50)
+            {
+                E = (A2 - 50) * 20;
+                B = 50 * 10;
+            }
+            else
+            {
+                B = A2 * 10;
+            }
+
+            int T = B + E;
+
+            Console.WriteLine($"Salário total: R$ {T}");
+            Console.WriteLine($"Salário excedente: R$ {E}");
+
+            Console.Write("Deseja encerrar o programa? (Digite S para SIM ou N para NÃO): ");
+            R = Console.ReadLine().ToUpper();
+
+        } while (R!= "S");
+    }
+}
