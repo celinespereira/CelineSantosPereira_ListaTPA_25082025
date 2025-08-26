15b18e1aab34859beff9e21e44eea8ee218b8cbc
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:35:58 -0300

    exercicio_3
    Escreva um programa que exiba na tela a tabuada de um número que deverá ser informado pelo usuário (deverá ser usada qualquer estrutura de repetição).

---
+++ exercicio_3/exercicio_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int A1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nTabuada do número {A1}:\n");
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"{A1} x {i} = {A1 * i}");
+        }
+    }
+}
