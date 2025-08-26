1f32ca8b143f6f24fda7a76b96a0651b11ca8d3d
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:44:16 -0300

    exercicio_9
    Escreva um programa que leia vários números inteiros e ao final informe quantos números pares, quantos números ímpares, quantos números positivos e quantos números negativos foram digitados pelo usuário. O programa só deve parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja encerrar o programa?".

---
+++ exercicio_9/exercicio_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A2 = 0, A3 = 0, A4 = 0, A5 = 0;
+        string R;
+
+        do
+        {
+            Console.Write("Digite um número: ");
+            int A1 = int.Parse(Console.ReadLine());
+
+            if (A1 % 2 == 0)
+                A2++;
+            else
+                A3++;
+
+            if (A1 > 0)
+                A4++;
+            else if (A1 < 0)
+                A5++;
+
+            Console.Write("Deseja encerrar o programa? (Digite S para SIM ou Digite N para NÃO): ");
+            R = Console.ReadLine().ToUpper();
+
+        } while (R != "S");
+
+        Console.WriteLine($"Quantidade de números Pares: {A2}");
+        Console.WriteLine($"Quantidade de números Ímpares: {A3}");
+        Console.WriteLine($"Quantidade de números Positivos: {A4}");
+        Console.WriteLine($"Quantidade de números Negativos: {A5}");
+    }
+}
