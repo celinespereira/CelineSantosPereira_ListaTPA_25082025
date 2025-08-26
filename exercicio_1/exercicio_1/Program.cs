848e21b1ba60f87aa0ebee631c70382b7214db9e
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:34:52 -0300

    exercicio_1
    Escreva um programa que exiba na tela em ordem crescente, apenas os números pares existentes de 11 a 250.

---
+++ exercicio_1/exercicio_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Números pares de 11 até 250 em ordem crescente:\n");
+
+        for (int i = 12; i <= 250; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
