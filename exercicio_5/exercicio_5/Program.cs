f1a13bd90763702e23510f1c321f671c3c731aa8
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:37:10 -0300

    exercicio_5
     Escreva um programa que leia 15 números inteiros e exiba na tela ao final, o maior número que foi digitado pelo usuário.

---
+++ exercicio_5/exercicio_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o 1º número: ");
+        int A1 = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= 15; i++)
+        {
+            Console.Write($"Digite o {i} número: ");
+            int A2 = int.Parse(Console.ReadLine());
+            if (A2 > A1) A1 = A2;
+        }
+
+        Console.WriteLine($"\nO maior número digitado foi: {A1}");
+    }
+}
