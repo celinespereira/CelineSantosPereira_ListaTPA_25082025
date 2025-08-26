c8eb746d402651b4dcc7bf1ebb02c6a5d0fb1185
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:35:21 -0300

    eercicio_2
    Escreva um programa que exiba na tela em ordem decrescente, apenas os números ímpares existentes entre dois números digitados pelo usuário (inclusive eles).

---
+++ exercicio_2/exercicio_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o primeiro número: \n");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: \n");
+        int b = int.Parse(Console.ReadLine());
+
+        for (int i = b; i >= a; i--)
+        {
+            if (i % 2 != 0) 
+            { 
+                Console.WriteLine(i); 
+            }
+        }
+            
+    }
+}
