5d36eea4644e408dc84cdcbc0bd201f5b191a439
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:38:14 -0300

    exercicio_6
    Escreva um programa que calcule e exiba a média de 10 notas digitadas pelo usuário. Considerar notas válidas, somente valores entre 0 (zero) e 10 (dez). Se o usuário digitar algum valor inválido, deverá ser exibida uma mensagem informando o ocorrido.

---
+++ exercicio_6/exercicio_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double B2 = 0;
+        int B1 = 0;
+
+        while (B1 < 10)
+        {
+            Console.Write($"Digite a nota {B1 + 1}: ");
+            double A1 = double.Parse(Console.ReadLine());
+
+            if (A1 >= 0 && A1 <= 10)
+            {
+                B2 += A1;
+                B1++;
+            }
+            else
+            {
+                Console.WriteLine("Nota inválida! Digite um valor entre 0 e 10.");
+            }
+        }
+
+        double M = B2 / 10;
+        Console.WriteLine($"\nA média das notas é: {M}");
+    }
+}
