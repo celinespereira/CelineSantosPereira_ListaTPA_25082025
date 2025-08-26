53b1bff552b0a14f953818bcdbe00dab513a6469
celinespereira <celinesantospereira7@gmail.com>
2025-08-25 22:39:02 -0300

    exercicio_7
     Escreva um programa que exiba todos os números de 1 a 100 e a cada número que for múltiplo de 10, exiba a mensagem “MÚLTIPLO DE 10”.

---
+++ exercicio_7/exercicio_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write(i);
+
+            if (i % 10 == 0)
+            {
+                Console.Write(" - MÚLTIPLO DE 10");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
